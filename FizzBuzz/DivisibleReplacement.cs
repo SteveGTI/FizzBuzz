59a74d19aa84c097c72a0799650d258b333e59fe
Steven Matthews <43808744+SteveGTI@users.noreply.github.com>
2020-02-29 21:08:40 +0000

    Add files via upload

---
+++ FizzBuzz/DivisibleReplacement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class DivisibleReplacement
+    {
+        public int Denominator;
+
+        public string Replacement;
+
+        // Constructor used by testing project
+        public DivisibleReplacement(int denominator, string replacement)
+        {
+            Denominator = denominator;
+            Replacement = replacement;
+        }
+
+        // Method to evaluate if the numerator parameter ought to be replaced when output
+        public string ReplacmentValue(int numerator)
+        {
+            return (numerator % Denominator == 0 ? Replacement : String.Empty);
+        }
+    }
+}
